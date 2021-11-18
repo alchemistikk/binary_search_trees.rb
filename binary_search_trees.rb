c19a701aeccc372818939d94b4d92b66f4d98a0d
Mike Mitchell <alchemistikk@gmail.com>
2021-11-18 10:13:23 -0500

    Define Node class

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -1,2 +1,16 @@
 # Do not use duplicate values
 arr = [1, 2, 3, 4, 5, 6, 7]
+
+class Node
+  include Comparable
+
+  def initalize(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
+
+  def <=>(other)
+    @data <=> other.data
+  end
+end
