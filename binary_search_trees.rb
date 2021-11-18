d4bb417af0d3a3f654853537854c67531d07ea9b
Mike Mitchell <alchemistikk@gmail.com>
2021-11-18 13:42:48 -0500

    Define #delete(value, root = @root)

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -36,12 +36,12 @@ class Tree
     root = Node.new(arr[middle])
     root.left = build_tree(arr[0...middle])
     root.right = build_tree(arr[middle + 1..-1])
-    root 
+    root
   end
 
   def insert(value, root = @root)
     return value if root.nil?
-  
+
     if value < root
       root.left = insert(value, root.left)
     else
@@ -50,9 +50,37 @@ class Tree
 
     root
   end
+
+  def delete(value, root = @root)
+    return root if root.nil?
+
+    if value < root
+      root.left = delete(value, root.left)
+    elsif value > root
+      root.right = delete(value, root.right)
+    elsif root.left.nil?
+      root = root.right
+    elsif root.right.nil?
+      root = root.left
+    else
+      root = min_value(root.right)
+      root.right = delete(root, root.right)
+    end
+
+    root
+  end
+
+  def min_value(node)
+    current = node
+    while current.left
+      current = current.left
+    end
+    current
+  end
 end
 
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 my_tree = Tree.new(arr)
 my_tree.insert(Node.new(12))
+my_tree.delete(Node.new(6345))
 my_tree.pretty_print
