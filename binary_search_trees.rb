caa55e785071a81ad85a530472f108ce5a83a1a7
Mike Mitchell <alchemistikk@gmail.com>
2021-11-18 11:11:37 -0500

    Define #build_tree for Tree class

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -1,10 +1,11 @@
 # Do not use duplicate values
-arr = [1, 2, 3, 4, 5, 6, 7]
+arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 class Node
   include Comparable
+  attr_accessor :data, :left, :right
 
-  def initalize(data)
+  def initialize(data)
     @data = data
     @left = nil
     @right = nil
@@ -16,9 +17,26 @@ class Node
 end
 
 class Tree
+  attr_accessor :root
+
   def initialize(arr)
-    @arr = arr
+    @root = build_tree(arr)
   end
 
-  @root = build_tree
+  def build_tree(raw)
+    return if raw.empty?
+
+    arr = raw.uniq.sort
+
+    middle = arr.length / 2
+
+    root = Node.new(arr[middle])
+    root.left = build_tree(arr[0...middle])
+    root.right = build_tree(arr[middle + 1..-1])
+    root 
+  end
 end
+
+my_tree = Tree.new(arr)
+puts my_tree.build_tree(arr)
+
